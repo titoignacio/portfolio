51b827a4e708684df0b1389412bcbc531b216d5b
Héctor Villalobos <h.villalobosm03@gmail.com>
2022-03-12 22:25:10 -0300

    my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+          backgroundImage: theme => ({
+              'body-pattern': "url('../img/micro_carbon.png')"
+
+          })  
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
